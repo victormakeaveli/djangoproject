bfa53eaa9db3e236a2fa1a3063465dc2bacaa4fa
victor hugo <victor.makeaveli@gmail.com>
2020-08-28 22:29:11 -0300

    implemented board message

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class post(models.Model):
+    text = models.TextField()
+    
+    def __str__(self):
+        """A string representation of the model."""
+        return self.text[:50]
+        
--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,12 @@
 from django.views.generic import TemplateView
 
+from .models import Post 
+
+
 class HomePageView(TemplateView):
     template_name = 'home.html'
 
 class AboutPageView(TemplateView):
+    model = Post
+
     template_name = 'about.html'

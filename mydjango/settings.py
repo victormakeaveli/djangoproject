1c82dc62543a2ac4c4400157cd3665aa52395a1a
Victor Hugo <supercyberloser@gmail.com>
2020-09-03 15:56:41 -0300

    minor bug fix

--- mydjango/settings.py
+++ mydjango/settings.py
@@ -118,5 +118,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/   
 
-STATIC_URL = '/mydjango/static/'
+STATIC_URL = './mydjango/static/'
 

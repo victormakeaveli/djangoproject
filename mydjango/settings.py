a844e51b6c0849535c5c7a471ee0e124bb7a6b16
Victor Hugo <supercyberloser@gmail.com>
2020-09-04 14:48:50 -0300

    bug fix

--- mydjango/settings.py
+++ mydjango/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = ')o^g7u8%czo3p%d))#(pi188dq9v(x43=xa)q!rlbw!!k4kz5%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 

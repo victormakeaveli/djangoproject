ef3798db379121de9cca7dc62067d24ab0036506
Victor Hugo <supercyberloser@gmail.com>
2020-09-07 13:59:56 -0300

    bug fix

--- mydjango/urls.py
+++ mydjango/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 from django.urls import path, include
 from django.conf.urls.static import static 
 from django.conf import settings
+from django.contrib import admin
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', include('pages.urls')),
 ]
 

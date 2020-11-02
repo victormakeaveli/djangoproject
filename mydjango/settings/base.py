ad34497390d1df186aeb16737a15b66195f168a9
Victor <victor.makeaveli@gmail.com>
2020-11-02 18:59:28 -0300

    configured settings properly

--- mydjango/settings/base.py
+++ mydjango/settings/base.py
@@ -34,7 +34,7 @@ ROOT_URLCONF = 'mydjango.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, '../templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -87,4 +87,4 @@ STATICFILES_DIRS = [
     ]
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_st')
+STATIC_ROOT = 'static_st'
